f4060250a20d1aabb982b5cb571e889158ec81d1
shrey <shreyashish@gmail.com>
2021-12-08 00:20:58 +0530

    I have modified the user credentials

--- TestData/LoginPageData.py
+++ TestData/LoginPageData.py
@@ -1,5 +1,5 @@
 class LoginPageData:
 
-     test_LoginPageData = [{"userid":"D0X51247","password":"Rm7E8dkmhq"}]
+     test_LoginPageData = [{"userid":"MALPANA","password":"DM@May2021"}]
      # [{"userid":"D0X51247","password":"Rm7E8dkmhq"}, {"userid":"D0X10987", "password":"ETHzNm8Kkr"}]
 
