fcc9c553a420b8fcefddfd5ca5e0f850c8a1def7
shrey <shreyashish@gmail.com>
2021-12-08 01:28:21 +0530

    I have modified the wrong CD

--- TestData/LoginPageData.py
+++ TestData/LoginPageData.py
@@ -1,5 +1,5 @@
 class LoginPageData:
 
-     test_LoginPageData = [{"userid":"MALPANA","password":"DM@May2021"}]
+     test_LoginPageData = [{"userid":"MALPANA1","password":"DM@May2021"}]
      # [{"userid":"D0X51247","password":"Rm7E8dkmhq"}, {"userid":"D0X10987", "password":"ETHzNm8Kkr"}]
 
